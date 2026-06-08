deb00074259956096f502553f42890ff5ad41724
Faried12 <112363906+Faried12@users.noreply.github.com>
2026-06-08 23:38:16 +0700

    database baru

--- DATABASE TERBARU 8 JUNI/speakverse.sql
+++ DATABASE TERBARU 8 JUNI/speakverse.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 31 Bulan Mei 2026 pada 10.45
+-- Waktu pembuatan: 08 Jun 2026 pada 18.37
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -32,6 +32,14 @@ CREATE TABLE `cache` (
   `value` mediumtext NOT NULL,
   `expiration` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `cache`
+--
+
+INSERT INTO `cache` (`key`, `value`, `expiration`) VALUES
+('laravel-cache-admin@simtas.test|127.0.0.1', 'i:1;', 1780882853),
+('laravel-cache-admin@simtas.test|127.0.0.1:timer', 'i:1780882853;', 1780882853);
 
 -- --------------------------------------------------------
 
@@ -99,6 +107,46 @@ CREATE TABLE `job_batches` (
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `lessons`
+--
+
+CREATE TABLE `lessons` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `unit_id` bigint(20) UNSIGNED NOT NULL,
+  `skill_type` enum('listening','reading','writing','speaking') NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `order_number` int(11) NOT NULL DEFAULT 1,
+  `status` enum('active','inactive') NOT NULL DEFAULT 'active',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `lessons`
+--
+
+INSERT INTO `lessons` (`id`, `unit_id`, `skill_type`, `title`, `description`, `order_number`, `status`, `created_at`, `updated_at`) VALUES
+(1, 2, 'listening', 'Listening', 'Listening lesson for UNIT 1', 1, 'active', '2026-06-07 20:45:33', '2026-06-07 20:45:33'),
+(2, 2, 'reading', 'Reading', 'Reading lesson for UNIT 1', 2, 'active', '2026-06-07 20:45:33', '2026-06-07 20:45:33'),
+(3, 2, 'writing', 'Writing', 'Writing lesson for UNIT 1', 3, 'active', '2026-06-07 20:45:33', '2026-06-07 20:45:33'),
+(4, 2, 'speaking', 'Speaking', 'Speaking lesson for UNIT 1', 4, 'active', '2026-06-07 20:45:33', '2026-06-07 20:45:33'),
+(5, 3, 'listening', 'Listening', 'Listening lesson for UNIT 2', 1, 'active', '2026-06-07 20:45:33', '2026-06-07 20:45:33'),
+(6, 3, 'reading', 'Reading', 'Reading lesson for UNIT 2', 2, 'active', '2026-06-07 20:45:33', '2026-06-07 20:45:33'),
+(7, 3, 'writing', 'Writing', 'Writing lesson for UNIT 2', 3, 'active', '2026-06-07 20:45:33', '2026-06-07 20:45:33'),
+(8, 3, 'speaking', 'Speaking', 'Speaking lesson for UNIT 2', 4, 'active', '2026-06-07 20:45:33', '2026-06-07 20:45:33'),
+(9, 4, 'listening', 'Listening', 'Listening lesson for UNIT 3', 1, 'active', '2026-06-07 20:45:33', '2026-06-07 20:45:33'),
+(10, 4, 'reading', 'Reading', 'Reading lesson for UNIT 3', 2, 'active', '2026-06-07 20:45:33', '2026-06-07 20:45:33'),
+(11, 4, 'writing', 'Writing', 'Writing lesson for UNIT 3', 3, 'active', '2026-06-07 20:45:33', '2026-06-07 20:45:33'),
+(12, 4, 'speaking', 'Speaking', 'Speaking lesson for UNIT 3', 4, 'active', '2026-06-07 20:45:33', '2026-06-07 20:45:33'),
+(13, 5, 'listening', 'Listening', 'Listening lesson for UNIT 4', 1, 'active', '2026-06-07 20:45:33', '2026-06-07 20:45:33'),
+(14, 5, 'reading', 'Reading', 'Reading lesson for UNIT 4', 2, 'active', '2026-06-07 20:45:33', '2026-06-07 20:45:33'),
+(15, 5, 'writing', 'Writing', 'Writing lesson for UNIT 4', 3, 'active', '2026-06-07 20:45:33', '2026-06-07 20:45:33'),
+(16, 5, 'speaking', 'Speaking', 'Speaking lesson for UNIT 4', 4, 'active', '2026-06-07 20:45:33', '2026-06-07 20:45:33');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `migrations`
 --
 
@@ -122,7 +170,11 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (7, '2026_05_25_014522_create_vocabulary_pretest_results_table', 4),
 (8, '2026_05_25_053303_add_category_to_vocabulary_pretests_table', 5),
 (9, '2026_05_31_054053_create_missions_table', 6),
-(10, '2026_05_31_054851_add_fields_to_missions_table', 7);
+(10, '2026_05_31_054851_add_fields_to_missions_table', 7),
+(11, '2026_06_08_030142_create_units_table', 8),
+(12, '2026_06_08_030901_create_lessons_table', 8),
+(13, '2026_06_08_035337_create_reading_materials_table', 9),
+(14, '2026_06_08_035402_create_reading_questions_table', 9);
 
 -- --------------------------------------------------------
 
@@ -143,13 +195,6 @@ CREATE TABLE `missions` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data untuk tabel `missions`
---
-
-INSERT INTO `missions` (`id`, `title`, `category`, `difficulty`, `reward_xp`, `description`, `deadline`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'New Missions', 'reading', 'medium', 89, 'New Mission Unlocked', '2026-06-03', 'active', '2026-05-30 23:48:14', '2026-05-30 23:48:14');
-
 -- --------------------------------------------------------
 
 --
@@ -161,6 +206,58 @@ CREATE TABLE `password_reset_tokens` (
   `token` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `reading_materials`
+--
+
+CREATE TABLE `reading_materials` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `lesson_id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `passage` longtext NOT NULL,
+  `instruction` text DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `reading_materials`
+--
+
+INSERT INTO `reading_materials` (`id`, `lesson_id`, `title`, `passage`, `instruction`, `image`, `created_at`, `updated_at`) VALUES
+(1, 2, 'Narrative Text', 'Long text...', 'Read the passage carefully', NULL, '2026-06-08 01:34:18', '2026-06-08 01:34:18');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `reading_questions`
+--
+
+CREATE TABLE `reading_questions` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `reading_material_id` bigint(20) UNSIGNED NOT NULL,
+  `question` text NOT NULL,
+  `option_a` varchar(255) NOT NULL,
+  `option_b` varchar(255) NOT NULL,
+  `option_c` varchar(255) NOT NULL,
+  `option_d` varchar(255) NOT NULL,
+  `option_e` varchar(255) DEFAULT NULL,
+  `correct_answer` enum('A','B','C','D','E') NOT NULL,
+  `score` int(11) NOT NULL DEFAULT 10,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `reading_questions`
+--
+
+INSERT INTO `reading_questions` (`id`, `reading_material_id`, `question`, `option_a`, `option_b`, `option_c`, `option_d`, `option_e`, `correct_answer`, `score`, `created_at`, `updated_at`) VALUES
+(2, 1, 'Whats the password', 'Environment', 'Technology', 'Animals', 'Pretty', 'Beauty', 'A', 10, '2026-06-08 08:51:37', '2026-06-08 08:51:37');
 
 -- --------------------------------------------------------
 
@@ -182,8 +279,37 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('DugqTWWJh2w9TRN8WqUDRCL3blW19RJ8L6NcMMAm', 2, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36', 'eyJfdG9rZW4iOiJKeHlNWkRSM0FLQ2x2Q2hwT0ZYdmhjOUJDUXVpM3doSFRjOW9vTEhtIiwiX3ByZXZpb3VzIjp7InVybCI6Imh0dHA6XC9cL3NwZWFrdmVyc2UudGVzdFwvYWRtaW5cL21pc3Npb25zIiwicm91dGUiOiJhZG1pbi5taXNzaW9ucyJ9LCJfZmxhc2giOnsib2xkIjpbXSwibmV3IjpbXX0sImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjoyfQ==', 1780166850),
-('kZLAYCchY5LG288TIV3IlyiJoW1FNfPESGyQZUV6', 2, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36', 'eyJfdG9rZW4iOiJ0Vkk2NWk5bXhzQU9sRjlvdm9sa3p3ZHJ0dVFBNGhqR2VLU3NvMTN5IiwiX3ByZXZpb3VzIjp7InVybCI6Imh0dHA6XC9cL3NwZWFrdmVyc2UudGVzdFwvYWRtaW5cL21pc3Npb25zIiwicm91dGUiOiJhZG1pbi5taXNzaW9ucyJ9LCJfZmxhc2giOnsib2xkIjpbXSwibmV3IjpbXX0sImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjoyfQ==', 1780211016);
+('HL246TUOeRlObHZhxBNBQbVG16YhzUJ9ZT5deATE', 2, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36', 'eyJfdG9rZW4iOiJJbWI1dGtyTlBUNDUxY0g5R1oxY2RZMWJYWTN5TzBoQXVzU3BtR2tCIiwidXJsIjpbXSwiX3ByZXZpb3VzIjp7InVybCI6Imh0dHA6XC9cL3NwZWFrdmVyc2UudGVzdFwvYWRtaW5cL3JlYWRpbmctbWF0ZXJpYWxzXC8yIiwicm91dGUiOiJhZG1pbi5yZWFkaW5nLW1hdGVyaWFscy5pbmRleCJ9LCJfZmxhc2giOnsib2xkIjpbXSwibmV3IjpbXX0sImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjoyfQ==', 1780936514);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `units`
+--
+
+CREATE TABLE `units` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `subtitle` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `order_number` int(11) NOT NULL,
+  `type` enum('pretest','unit','posttest') NOT NULL,
+  `status` enum('active','inactive') NOT NULL DEFAULT 'active',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `units`
+--
+
+INSERT INTO `units` (`id`, `title`, `subtitle`, `description`, `order_number`, `type`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'PRE-TEST', 'Baseline Assessment', NULL, 1, 'pretest', 'active', '2026-06-07 20:31:13', '2026-06-07 20:31:13'),
+(2, 'UNIT 1', 'Are We Connected to Nature?', NULL, 2, 'unit', 'active', '2026-06-07 20:31:13', '2026-06-07 20:31:13'),
+(3, 'UNIT 2', 'Discovering Ourselves', NULL, 3, 'unit', 'active', '2026-06-07 20:31:13', '2026-06-07 20:31:13'),
+(4, 'UNIT 3', 'Why is Water Important?', NULL, 4, 'unit', 'active', '2026-06-07 20:31:13', '2026-06-07 20:31:13'),
+(5, 'UNIT 4', 'Why Should We Live a Healthy Life?', NULL, 5, 'unit', 'active', '2026-06-07 20:31:13', '2026-06-07 20:31:13'),
+(6, 'POST-TEST', 'Final Assessment', NULL, 6, 'posttest', 'active', '2026-06-07 20:31:13', '2026-06-07 20:31:13');
 
 -- --------------------------------------------------------
 
@@ -238,7 +364,9 @@ CREATE TABLE `vocabulary_pretests` (
 
 INSERT INTO `vocabulary_pretests` (`id`, `question`, `category`, `option_a`, `option_b`, `option_c`, `option_d`, `option_e`, `correct_answer`, `created_at`, `updated_at`) VALUES
 (1, 'apa itu vocab...', 'vocabulary', 'eat', 'eater', 'eating', 'at', 'eated', 'A', '2026-05-25 02:08:38', '2026-05-25 02:08:38'),
-(2, 'lagi', 'vocabulary', 'lagi', 'lagi', 'lagi', 'lagi', 'lagi', 'A', '2026-05-25 02:11:13', '2026-05-25 02:11:13');
+(2, 'lagi', 'vocabulary', 'lagi', 'lagi', 'lagi', 'lagi', 'lagi', 'A', '2026-05-25 02:11:13', '2026-05-25 02:11:13'),
+(3, 'Question 1 TEST', 'vocabulary', 'Test A', 'Test B', 'Test C', 'Test D', 'Test E', 'C', '2026-06-03 04:02:38', '2026-06-03 04:02:38'),
+(4, 'pp', 'reading', 'ppp', 'ppp', 'ppp', 'ppp', 'pp', 'A', '2026-06-04 20:11:21', '2026-06-04 20:11:21');
 
 -- --------------------------------------------------------
 
@@ -253,6 +381,14 @@ CREATE TABLE `vocabulary_pretest_results` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `vocabulary_pretest_results`
+--
+
+INSERT INTO `vocabulary_pretest_results` (`id`, `user_id`, `score`, `created_at`, `updated_at`) VALUES
+(1, 1, 0, '2026-06-04 18:47:43', '2026-06-04 18:47:43'),
+(2, 1, 0, '2026-06-04 18:47:47', '2026-06-04 18:47:47');
 
 --
 -- Indexes for dumped tables
@@ -293,6 +429,14 @@ ALTER TABLE `job_batches`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `lessons`
+--
+ALTER TABLE `lessons`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `lessons_unit_id_foreign` (`unit_id`),
+  ADD KEY `lessons_skill_type_index` (`skill_type`);
+
+--
 -- Indeks untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
@@ -311,12 +455,33 @@ ALTER TABLE `password_reset_tokens`
   ADD PRIMARY KEY (`email`);
 
 --
+-- Indeks untuk tabel `reading_materials`
+--
+ALTER TABLE `reading_materials`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `reading_materials_lesson_id_foreign` (`lesson_id`);
+
+--
+-- Indeks untuk tabel `reading_questions`
+--
+ALTER TABLE `reading_questions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `reading_questions_reading_material_id_foreign` (`reading_material_id`);
+
+--
 -- Indeks untuk tabel `sessions`
 --
 ALTER TABLE `sessions`
   ADD PRIMARY KEY (`id`),
   ADD KEY `sessions_user_id_index` (`user_id`),
   ADD KEY `sessions_last_activity_index` (`last_activity`);
+
+--
+-- Indeks untuk tabel `units`
+--
+ALTER TABLE `units`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `units_order_number_index` (`order_number`);
 
 --
 -- Indeks untuk tabel `users`
@@ -355,16 +520,40 @@ ALTER TABLE `jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT untuk tabel `lessons`
+--
+ALTER TABLE `lessons`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
 -- AUTO_INCREMENT untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT untuk tabel `missions`
 --
 ALTER TABLE `missions`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `reading_materials`
+--
+ALTER TABLE `reading_materials`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `reading_questions`
+--
+ALTER TABLE `reading_questions`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `units`
+--
+ALTER TABLE `units`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
@@ -376,17 +565,35 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `vocabulary_pretests`
 --
 ALTER TABLE `vocabulary_pretests`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `vocabulary_pretest_results`
 --
 ALTER TABLE `vocabulary_pretest_results`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
+
+--
+-- Ketidakleluasaan untuk tabel `lessons`
+--
+ALTER TABLE `lessons`
+  ADD CONSTRAINT `lessons_unit_id_foreign` FOREIGN KEY (`unit_id`) REFERENCES `units` (`id`) ON DELETE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `reading_materials`
+--
+ALTER TABLE `reading_materials`
+  ADD CONSTRAINT `reading_materials_lesson_id_foreign` FOREIGN KEY (`lesson_id`) REFERENCES `lessons` (`id`) ON DELETE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `reading_questions`
+--
+ALTER TABLE `reading_questions`
+  ADD CONSTRAINT `reading_questions_reading_material_id_foreign` FOREIGN KEY (`reading_material_id`) REFERENCES `reading_materials` (`id`) ON DELETE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `vocabulary_pretest_results`
